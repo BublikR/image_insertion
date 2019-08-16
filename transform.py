cdfdd43604e501a414dc1c1ff5e6b760e3f52387
Roman Bublik <sir.bublik@gmail.com>
2019-08-16 17:20:27 +0300

    remove mask_gauss and change thresholds in cv2.Canny()

--- transform.py
+++ transform.py
@@ -1,18 +1,19 @@
-import cv2 
 import numpy as np
+import cv2
 input_img = cv2.imread('input2.jpg')
 Yo, Xo, _ = input_img.shape
 insert_img = cv2.imread('insert.jpg')
 Y, X, _ = insert_img.shape
 
+# color space change
 hsv = cv2.cvtColor(input_img, cv2.COLOR_BGR2HSV)
-
+# shades color limits
 lower_green = np.array([50, 50, 50])
 upper_green = np.array([70, 255, 255])
-
+# figure selection
 mask = cv2.inRange(hsv, lower_green, upper_green)
-mask_gauss = cv2.GaussianBlur(mask, (19, 19), 0)
-edged = cv2.Canny(mask_gauss, 10, 250)
+edged = cv2.Canny(mask, 1000, 1500)
+
 kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (7, 7))
 closed = cv2.morphologyEx(edged, cv2.MORPH_CLOSE, kernel)
 
@@ -21,12 +22,10 @@ for c in cnts:
     peri = cv2.arcLength(c, True)
     approx = cv2.approxPolyDP(c, 0.02*peri, True)
     if len(approx) == 4:
-        #cv2.drawContours(input_img, approx, -1, (255, 0, 0), 4)
         pts1 = np.float32([[0, 0],[X, 0],[0, Y],[X, Y]])
         pts2 = np.array([i[0] for i in approx])
         pts2 = np.float32(pts2)
         pts2[0], pts2[1] = pts2[1].copy(), pts2[0].copy()
-        print(pts2)
 
         if pts2[0][1] == np.min(np.min(pts2, axis=1)):
             temp = pts2[1:].copy()
@@ -37,8 +36,6 @@ for c in cnts:
             if not temp[0][1] == np.min(np.min(temp, axis=1)):
                 pts2[0], pts2[1], pts2[2], pts2[3] = pts2[1].copy(), pts2[3].copy(), pts2[0].copy(), pts2[2].copy()
 
-        #pts2 = np.float32(pts2)
-        print(pts2)
         M = cv2.getPerspectiveTransform(pts1, pts2)
         dst = cv2.warpPerspective(insert_img, M, (Xo, Yo))
 
@@ -54,6 +51,7 @@ for c in cnts:
         input_img[np.all(input_img == [0, 0, 0, 255], axis=2)] = [0, 0, 0, 0]
 
         added_image = cv2.addWeighted(input_img, 1, dst, 1, 0)
+
 cv2.imshow('result', added_image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
